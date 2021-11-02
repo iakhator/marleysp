49a80be69d69af8b3f7bc367c27d10bfcf114577
iakhator <iakhator@gmail.com>
2021-11-02 18:52:03 +0100

    chore: add readme

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,11 +4,11 @@ const NavBar = () => {
  return (
    <Navbar bg="light">
     <Container>
-      <Navbar.Brand href="#home">Recipe</Navbar.Brand>
+      <Navbar.Brand href="#home">Recipe Spoon</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/">Recipes</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
